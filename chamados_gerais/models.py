5e2781ffbf9be1cd5c5ab93a8b01bd545e658e7c
Paull20-20 <paulloholiveira77@gmail.com>
2022-03-18 15:58:41 -0300

    .

--- chamados_gerais/models.py
+++ chamados_gerais/models.py
@@ -1,6 +1,10 @@
 from turtle import title
 from django.db import models
 from django.contrib.auth import get_user_model
+#import matplotlib matplotlib.use('Agg')  # Must be done before importing pyplot import matplotlib.pyplot as plt
+import tkinter
+top = tkinter.Tk();
+top = tkinter.mainloop();
 
 # Create your models here. esse
 
